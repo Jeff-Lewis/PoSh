d84c6095b60d560e90362e8d62598cf618087a1c
User <User@04c1ca97-9b13-ea49-9522-9e354e993536>
2016-03-18 07:45:20 +0000

    * Get-ConnectionString обработка oleDB, без наворотов
    git-svn-id: file:///c/users/voronka/svnrepositories/powershell/branches/sql4posh@10 04c1ca97-9b13-ea49-9522-9e354e993536

---
+++ Modules/SQL4PoSh/SQL4PoSh.psm1
@@ -0,0 +1,140 @@
+﻿function Get-ConnectionString {
+    [CmdletBinding()]
+    param (
+        [parameter(
+            Mandatory = $true,
+            HelpMessage = 'Enter server name.'
+        )]
+        [string]$server,
+
+        [parameter(
+            HelpMessage = 'If use not default instance, enter name.'
+        )]
+        [string]$instance,
+
+        [parameter(
+            Mandatory = $true,
+            HelpMessage = 'Enter database name.'
+            )]
+        [string]$database,
+
+        [parameter(
+            ParameterSetName = 'NativeUser',
+            HelpMessage = 'Enter user name.'
+        )]
+        [string]$user,
+
+        [parameter(
+            ParameterSetName = 'NativeUser',
+            HelpMessage = 'Enter password.'
+        )]
+        [string]$password,
+
+        [parameter(
+            ParameterSetName = 'NTLMUser',
+            HelpMessage = 'If you use trusted connection method, use this switch. User/password option not use.'
+        )]
+        [Alias('trustedUser')]
+        [switch]$trustedConnection,
+
+        [parameter(
+            HelpMessage = 'if connect with OLE DB, use this option'
+        )]
+        [ValidateSet('Access', 'Active Directory', 'MySQL', 'Oracle')]
+        
+        [string]$oledb,
+        [parameter()]
+        [string]$datasource
+
+
+    )
+    process {
+        [string]$string = $null;
+        if (![String]::IsNullOrEmpty($oledb)) {
+            switch ($oledb) {
+                'Oracle' {
+                    $string += "Provider=OraOLEDB.Oracle; Date Source=$datasource; ";
+                    if ($trustedConnection.IsPresent) {
+                        $string += 'OSAuthent=1; ';
+                    }
+                    else {
+                        $string += "User Id=$user; Password=$password; ";
+                    }
+                }
+                'Access' {
+                    $string += "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=$datasource; ";
+                    if ($trustedConnection.IsPresent) {
+                        $string += "Persist Security Info=False; ";
+                    }
+                    else {
+                        $string += "Jet OLEDB:Database Password=$password; ";
+                    }
+                }
+                'Active Directory' {
+                    $string += 'Provider=ADSDSOObject; ';
+                    if (!$trustedConnection.IsPresent) {
+                        $string += "User Id=$user;Password=$password; "
+                    }
+                }
+                'MySQL' {
+                    $string += "Provider=MySQLProv; Data Source=$datasource; ";
+                    $string += "Uid=$user; Pwd=$password; ";
+                }
+
+            } 
+        }
+        else {
+            $string += "Server=$server";
+            if (![String]::IsNullOrEmpty($instance)) {
+                $string += "\$instance";
+            }
+            $string += "; Database=$database; "
+            if ($trustedConnection.IsPresent) {
+                $string += "Trusted_Connection=True;"   
+            }
+            else {
+                $string += "User Id=$user; Password=$password;"
+            }
+        }
+
+        return $string;
+    }
+
+}
+
+function Get-SQLData {
+    [CmdletBinding()]
+    param (
+        [string]$connsetcionString,
+        [string]$query,
+        [switch]$isSQLServer
+    )
+    begin {
+        [System.Data.Common.DbConnection]$connection = $null;
+        if ($isSQLServer.IsPresent) {
+            $connection = New-Object -TypeName System.Data.SqlClient.SqlConnection;
+        }
+        else {
+            $connection = New-Object -TypeName System.Data.OleDb.OleDbConnection;
+        }
+        
+        $connection.ConnectionString = $connsetcionString;
+        
+        [System.Data.Common.DbCommand]$command = $connection.CreateCommand();
+    }
+
+    process {
+        $command.CommandText = $query;
+        
+        [System.Data.Common.DataAdapter]$adapter = $null;
+        if ($isSQLServer.IsPresent) {
+            $adapter = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter($command);
+        }
+        else {
+            $adapter = New-Object -TypeName System.Data.OleDb.OleDbDataAdapter($command);
+        }
+
+        [System.Data.DataSet]$dataSet = New-Object -TypeName System.Data.DataSet;
+        $adapter.Fill($dataSet);
+    }
+}
