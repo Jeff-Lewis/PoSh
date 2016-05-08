51360e7d93076bf24b959b581a429da54e076dd8
Vladimir Chistiakov <vovochko@gmail.com>
2016-05-08 21:34:14 +0300

    Change to test functions

--- Modules/7z4posh/7z4posh.tests.ps1
+++ Modules/7z4posh/7z4posh.tests.ps1
@@ -4,8 +4,19 @@ $env:PSModulePath = $env:PSModulePath.Insert(0, (Split-Path -Path $here -Parent)
 $name = $MyInvocation.MyCommand.Name.Split('.')[0];
 Import-Module $name -Force;
 
-$z = Create-7zipper;
-Benchmark-7z -zipper $z -verbouse;
-$z;
-#$z.process.args = 'b 1';
-#$z.RunSync();
+
+function test1 {
+	Write-Host "Test 1: default benchmark";
+	$z = Create-7zipper;
+	Benchmark-7z -zipper $z -verbouse;
+	$z;
+}
+
+function test2 {
+	Write-Host "Test 2: List archive";
+	$z = Create-7zipper;
+	List-7z -zipper $z -path '../../7ztest.7z';
+	$z.process;
+}
+
+test2;
