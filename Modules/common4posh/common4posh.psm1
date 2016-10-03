0361cbadcc3eab86ce3f58fc533cc7adce78f594
Vladimir Chistiakov <VVChistyakov.SBT@sberbank.ru>
2016-10-03 11:43:48 +0300

    common4posh * Get-ItemPath trow exception + Help for cmdlet Get-ItemPath

--- Modules/common4posh/common4posh.psm1
+++ Modules/common4posh/common4posh.psm1
@@ -1,4 +1,27 @@
+<#
+.SYNOPSIS
+Advanced Get-Item cmdlet
+.DESCRIPTION
+Get the item from the relative path of the working directory or by specifying the parent directory.
+.PARAMETER path
+Item path or name.
+.PARAMETER parentPath
+Parent item path or name.
+.EXAMPLE
+$item = Get-ItemPath -path 'c:\temp';
+Get item from path c:\temp
+.EXAMPLE
+$item = Get-ItemPath -Path 'windows' -parentPath 'c:' ;
+Get item 'windows' from parent 'c:\'
+.INPUTS
+System.String. You can input in pipe search name of items.
+.OUTPUTS
+System.IO.FileInfo
+System.IO.DirectoryInfo
+System.IO.IOException
+#>
 function Get-ItemPath {
+	
 	[CmdletBinding()]
 	param(
 		[Parameter(Mandatory = $true, ValueFromPipeLine = $true)]
@@ -13,7 +36,7 @@ function Get-ItemPath {
 		switch ($true) {
 			{$path.Length -eq 0} {
 				return $null;
-			}
+			} 
 			{$path | Test-Path} {
 				return $path | Get-Item;
 			}
@@ -22,8 +45,13 @@ function Get-ItemPath {
 				if ($joingPath | Test-Path) {
 					return $joingPath | Get-Item;		
 				}
+				else {
+					throw [System.IO.IOException] "Path $joingPath not found";
+					return $null;
+				}
 			}
 			default {
+				throw [System.IO.IOException] "Path $path not found";
 				return $null;		
 			}
 		}
